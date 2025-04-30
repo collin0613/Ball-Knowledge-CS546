8813a181138090fe4450e083e24bfd5f217eb695
Josh Bekkerman <joshbekkerman@Joshs-Laptop.local>
2025-04-29 20:19:43 -0400

    created leaderboard page and data function

--- src/routes/views/index.js
+++ src/routes/views/index.js
@@ -1,10 +1,12 @@
 import userpageRoutes from './users.js';
 import homepageRoutes from './home.js';
+import leaderboardRoutes from './leaderboard.js';
 import { Router } from 'express';
 const router = Router();
 
 router.use('/', homepageRoutes);
 router.use('/account', userpageRoutes);
+router.use('/leaderboard', leaderboardRoutes);
 
 router.use('*', (req, res) => {
     return res.status(404).json({ error: 'Not found' });
